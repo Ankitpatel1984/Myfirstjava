367f6edb5d63e3d777d7b6f0956b9777ff8f2c29
Ankit Patel <ankitpatel1984@gmail.com>
2023-10-28 14:29:50 +0100

    modified my java

--- MyProjectFour.java
+++ MyProjectFour.java
@@ -1,4 +1,11 @@
 package myprojectfour;
 
 public class MyProjectFour {
+    public static void main(String[] args) {
+        int a=20;
+        int b=46;
+        System.out.println(a+b);
+
+    }
 }
+
